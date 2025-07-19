470762159b4013d3c3a4f91da6ea694c6079b015
B41Bye <kidstas8@gmail.com>
2025-07-19 17:32:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Отправляем данные на сервер при загрузке страницы
-fetch("https://ваш-сервер.com/visit", {
+fetch("https://010101.pythonanywhere.com/", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
